7fbe39ccd1513f9973b3a09032f8eab33f10807c
Ahmed Medra <ahmed.medrah@gmail.com>
2021-07-18 18:48:51 +0200

    Create largest_common_subsequence.py

---
+++ largest_common_subsequence.py
@@ -0,0 +1,22 @@
+word1 = 'blue'
+word2 = 'clues'
+
+def longest_substring(w1,w2):
+    # create an empty grid w1 length by w2 length
+    grid = [[0]*len(w2) for i in range(len(w1))]
+    for i in range(len(w1)):
+        for j in range(len(w2)):
+            # check if the letters match
+            if w1[i] == w2[j]:
+                # set the cell corresponding to the match to the value of the last match +1                          
+                last = grid[i-1][j-1] +1
+                grid[i][j] = last  
+            else: 
+                # set the cell to the largest of top and left cell
+                top = grid[i-1][j]  
+                left = grid[i][j-1] 
+                grid[i][j] = max(top,left)
+    # return the largest match            
+    return max(max(grid))
+
+print(longest_substring(word1,word2))                    
